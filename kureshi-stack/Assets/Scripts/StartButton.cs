c2090cab6b63dadbe9466a757b2932dfd8eef1be
sam <samcreate0830@gmail.com>
2018-03-03 00:49:31 +0900

    タイトルシーンとゲームシーンのつなぎ

---
+++ kureshi-stack/Assets/Scripts/StartButton.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartButton : MonoBehaviour {
+
+	public void OnClick() {
+		// TODO:次のシーンに移動
+		Debug.Log("ボタンを押した");
+		GameSceneManager.Instance.LoadGameScene();
+	}
+}
